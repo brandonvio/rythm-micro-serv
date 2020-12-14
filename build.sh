6d95ccfb0f0e80f6149be8077e1b0ddbe69bbeb3
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-12-13 16:36:30 -0800

    Development.

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+cd rythm-price-micro-serv
+docker build -t rythm/rythem-price-micro-serv .
+
+cd ..
+
 cd rythm-micro-serv-cdk
 npm install
 npm run build
--- rythm-price-micro-serv/build.sh
+++ rythm-price-micro-serv/build.sh
@@ -1,2 +1,2 @@
 docker build -t rythm/rythem-price-micro-serv .
-docker run --rm -it -p 8000:8000 rythm/rythem-price-micro-serv .
+# docker run --rm -it -p 8000:8000 rythm/rythem-price-micro-serv .
