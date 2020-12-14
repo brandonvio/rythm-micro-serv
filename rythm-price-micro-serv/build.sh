de5f1157f390ad740efc05938995dff0eea6aea8
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-12-13 20:47:44 -0800

    Development.

--- rythm-price-micro-serv/build.sh
+++ rythm-price-micro-serv/build.sh
@@ -1,2 +1,6 @@
-docker build -t rythm/rythem-price-micro-serv .
-docker run --rm -it -p 8000:8000 rythm/rythem-price-micro-serv .
+aws ecr get-login-password --region us-west-2 --profile scratch-account-01 | docker login --username AWS --password-stdin 778477161868.dkr.ecr.us-west-2.amazonaws.com
+# aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 778477161868.dkr.ecr.us-west-2.amazonaws.com
+docker build -t rythm-price-repository .
+docker tag rythm-price-repository:latest 778477161868.dkr.ecr.us-west-2.amazonaws.com/rythm-price-repository:latest
+docker push 778477161868.dkr.ecr.us-west-2.amazonaws.com/rythm-price-repository:latest
+# docker run --rm -it -p 8000:8000 rythm/rythem-price-micro-serv .
