7ab432964ed3dab6b4f67b04e844d2a09e8e03b5
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-12-13 20:43:46 -0800

    Development.

--- rythm-micro-serv-cdk/test/rythm-micro-serv.test.ts
+++ rythm-micro-serv-cdk/test/rythm-micro-serv.test.ts
@@ -1,13 +1,18 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as RythmMicroServ from '../lib/rythm-micro-serv-stack';
+import { expect as expectCDK, matchTemplate, MatchStyle } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as RythmMicroServ from "../lib/_rythm-micro-serv-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new RythmMicroServ.RythmMicroServStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new RythmMicroServ.RythmMicroServStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
