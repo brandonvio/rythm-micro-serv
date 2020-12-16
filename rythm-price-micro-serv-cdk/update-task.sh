85c8bf6033bdac38ca6028deb8e363b159b5c9ab
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-12-15 20:29:22 -0800

    Update the service.

--- rythm-price-micro-serv-cdk/update-task.sh
+++
@@ -1,3 +0,0 @@
-ecsClusterName=RythmClusterUpdate
-
-aws ecs list-tasks --cluster $ecsClusterName --desired-status RUNNING --family ${nameTaskDefinition} | egrep "task/" | sed -E "s/.*task\/(.*)\"/\1/"
