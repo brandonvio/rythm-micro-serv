d2f0e4d4f78853769da8e9135dc28627a89388fc
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-12-15 21:15:48 -0800

    Development.

--- rythm-price-micro-serv/update_service.sh
+++ rythm-price-micro-serv/update_service.sh
@@ -3,6 +3,10 @@ ecs_cluster=RythmCluster
 ecs_service=PriceService
 aws_profile=scratch-account-01
 
-aws ecs update-service --desired-count 0 --cluster $ecs_cluster --service $ecs_service --profile $aws_profile
-aws ecs wait services-stable --cluster $ecs_cluster --services $ecs_service --profile $aws_profile
-aws ecs update-service --desired-count 1 --cluster $ecs_cluster --service $ecs_service --force-new-deployment --profile $aws_profile
+# aws ecs update-service --desired-count 0 --cluster $ecs_cluster --service $ecs_service --profile $aws_profile
+# aws ecs wait services-stable --cluster $ecs_cluster --services $ecs_service --profile $aws_profile
+# aws ecs update-service --desired-count 1 --cluster $ecs_cluster --service $ecs_service --force-new-deployment --profile $aws_profile
+
+aws ecs update-service --desired-count 0 --cluster $ecs_cluster --service $ecs_service
+aws ecs wait services-stable --cluster $ecs_cluster --services $ecs_service
+aws ecs update-service --desired-count 1 --cluster $ecs_cluster --service $ecs_service --force-new-deployment
